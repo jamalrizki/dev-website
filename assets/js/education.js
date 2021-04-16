3c20b175ec65825268e141101ab3f6f5ba36bf2c
Jamal Rizki <jamalbrizki@gmail.com>
2021-04-16 12:09:06 -0500

    first commit

---
+++ assets/js/education.js
@@ -0,0 +1,255 @@
+AOS.init();
+
+// MOOCs Cards
+
+const moocs = document.querySelector(".moocs");
+const moocscards = [
+  {
+    title: "Google UX Design Certificate",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/professional-certificates/google-ux-design",
+  },
+  {
+    title: "Learn Ethical Hacking From Scratch",
+    cardImage: "assets/images/education-page/udemy.png",
+    moocLink: "https://www.udemy.com/course/learn-ethical-hacking-from-basics/",
+  },
+  {
+    title: "Facebook Social Media Marketing",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/professional-certificates/facebook-social-media-marketing",
+  },
+  {
+    title: "Google IT Support",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/professional-certificates/google-it-support?",
+  },
+  {
+    title: "Graphic Design For Non-Designers \nUniversity of Colorado",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/specializations/graphic-design-elements-non-designers",
+  },
+  {
+    title: "Facebook Certified Digital Marketing Associate",
+    cardImage: "assets/images/education-page/fb2.png",
+    moocLink: "https://www.facebook.com/business/learn/certification/exams/100-101-exam",
+  },
+  {
+    title: "WSET Level II in Wine and Spirits",
+    cardImage: "assets/images/education-page/wset.png",
+    moocLink: "https://www.wsetglobal.com/qualifications/wset-level-2-award-in-wines",
+  },
+  {
+    title: "Level 3 Teach English As A Foreign Language (TEFL)",
+    cardImage: "assets/images/education-page/english.png",
+    moocLink: "https://www.i-to-i.com/teaching-english-as-a-foreign-language.html",
+  },
+  {
+    title: "Photography Basics And Beyond <br>University of Michigan",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/specializations/photography-basics?",
+  },
+  {
+    title: "Front End Development With React <br>Hong Kong University",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/learn/front-end-react",
+  },
+  {
+    title: "Javascript Data Structures And Algorithms",
+    cardImage: "assets/images/education-page/udemy.png",
+    moocLink: "https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/",
+  },
+  {
+    title: "Excel Skills For Business: Essentials <br>Macquarie University",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/learn/excel-essentials?specialization=excel",
+  },
+  {
+    title: "Cert 3 in Hospitality",
+    cardImage: "assets/images/education-page/hospo.jpg",
+    moocLink: "https://smartandskilled.nsw.gov.au/sands/traineeship/SIT30616?meta_l_sand=91054&meta_w_sand=108",
+  },
+  {
+    title: "Classical Music <br>Yale University",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/learn/introclassicalmusic",
+  },
+  {
+    title: "Cert 4 in Small Business Management",
+    cardImage: "assets/images/education-page/tafe.jpg",
+    moocLink: "https://www.myskills.gov.au/courses/details?Code=BSB42518",
+  },
+  {
+    title: "Financial Markets <br>Yale University",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/learn/financial-markets-global",
+  }
+];
+
+const experience = [
+  {
+    img: "assets/images/education-page/c1.png"
+  },
+  {
+    img: "assets/images/education-page/c2.jpg"
+  },
+  {
+    img: "assets/images/education-page/c3.png"
+  },
+  {
+    img: "assets/images/education-page/c4.png"
+  },
+  {
+    img: "assets/images/education-page/c5.jpg"
+  },
+];
+
+let currentItem = 0;
+
+const img = document.getElementById('image');
+
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+window.addEventListener('DOMContentLoaded', function () {
+  showExperience();
+})
+
+function showExperience() {
+  setInterval(function () {
+    if (currentItem === experience.length) {
+      currentItem = 0;
+    }
+      const item = experience[currentItem];
+      img.src = item.img;
+      currentItem++;
+
+    },
+    3000);
+}
+
+const showCards = () => {
+  let output = "";
+  moocscards.forEach(
+    ({ title, cardImage, moocLink }) =>
+    (output += `        
+        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >  
+            <div class="card mb-3 mx-auto">
+               <div class="content">
+                  <div class="content-overlay"></div>
+                    <img src=${cardImage} class="card-img-top content-image">     
+                  <div class="content-details fadeIn-bottom">
+                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
+                  </div>
+                </div>
+                <div class="card-body">
+                    <h6 class="mt-0 py-2 text-center font-weight-bold mooc-title" style="font-size:12px;">${title}</h6>
+                </div>
+            </div>
+        </div>        
+      `)
+  );
+  moocs.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+/* Badges*/
+
+const bagdes = document.querySelector(".badges");
+const badgesection = [
+  {
+    title: "Google Developer Essentials",
+    image: "assets/images/education-page/badge1.png",
+    description: "Earned May 20, 2020",
+  },
+  {
+    title: "VM Migration",
+    image: "assets/images/education-page/badge2.png",
+    description: "Earned June 20, 2020",
+  },
+  {
+    title: "G Suite Essentials",
+    image: "assets/images/education-page/badge3.png",
+    description: "Earned July 20, 2020",
+  },
+];
+
+const showCards1 = () => {
+  let output = "";
+  badgesection.forEach(
+    ({ title, image, description }) =>
+    (output += `       
+      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600"> 
+        <img class="img-fluid d-block mb-3 mx-auto hvr-grow" src="${image}" alt="Card image cap" width="200">
+          <div class="text-center font-weight-bolder" style="font-size: 1.3em;">${title}</div>
+          <div class="text-center text-muted font-weight-bolder p-2">${description}</div>
+      </div>`)
+  );
+  bagdes.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards1);
+
+
+/* Timeline Section*/
+
+const timeline = document.querySelector(".timeline");
+const timelinesection = [
+  {
+    heading: "Massachusetts Institute of Technology",
+    image: "./assets/images/education-page/timeline-1.svg",
+    subheading: "Bachelors of Science, Computer Science & Engineering, (2020-2024)",
+    description: "<li>Top 1% in the department</li><li>Best undergraduate Reseracher</li><li>Betsy Schumacker Woman</li><li>Athlete of the Year Award</li>",
+  },
+
+  {
+    heading: "Massachusetts Institute of Technology",
+    image: "./assets/images/education-page/timeline-1.svg",
+    subheading: "Bachelors of Science, Computer Science & Engineering, (2020-2024)",
+    description: "<li>Top 1% in the department</li><li>Best undergraduate Reseracher</li><li>Betsy Schumacker Woman</li><li>Athlete of the Year Award</li>",
+  },
+
+  {
+    heading: "Massachusetts Institute of Technology",
+    image: "./assets/images/education-page/timeline-1.svg",
+    subheading: "Bachelors of Science, Computer Science & Engineering, (2020-2024)",
+    description: "<li>Top 1% in the department</li><li>Best undergraduate Reseracher</li><li>Betsy Schumacker Woman</li><li>Athlete of the Year Award</li>",
+  },
+
+  {
+    heading: "Massachusetts Institute of Technology",
+    image: "./assets/images/education-page/timeline-1.svg",
+    subheading: "Bachelors of Science, Computer Science & Engineering, (2020-2024)",
+    description: "<li>Top 1% in the department</li><li>Best undergraduate Reseracher</li><li>Betsy Schumacker Woman</li><li>Athlete of the Year Award</li>",
+  },
+];
+
+const showCards2 = () => {
+  let output = "";
+  timelinesection.forEach(
+    ({ heading, image, subheading, description }) =>
+    (output += `       
+      <div class="timeline" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600">
+      <div class="timeline-content">
+        <h6 class="center-align font-weight-bolder pt-1">${heading}</h6>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-4">
+            <img src="${image}" alt="" height="110"
+              class=" mt-2 w-100 mx-auto">
+          </div>
+          <div class="col-lg-8 col-md-8 col-sm-8 px-4">
+            <span style="opacity: 0.9; font-size: 15px;">
+              ${subheading}
+            </span>
+            <h6 class="text-muted mt-2" style="font-size: small;">Activities:</h6>
+            <ol style="margin-top: -8px; margin-left: -10px; font-size: 12px; list-style: disc;">
+              ${description}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>`)
+  );
+  timeline.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
---
+++ assets/js/experience.js
@@ -0,0 +1,194 @@
+//  Work experience cards
+
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "Student",
+    cardImage: "assets/images/experience-page/study3.jpg",
+    place: "Remote Study",
+    time: "(March, 2020 - present)",
+    desp: "<li>Completed a Full Stack coding bootcamp.</li> <li>Completed courses several areas including Ux design, It Support, Social Media, Cybersecurity and Design .</li>",
+  },
+  {
+    title: "Freelance Developer",
+    cardImage: "assets/images/experience-page/free5.jpg",
+    place: "Remote",
+    time: "(Jan, 2021 - present)",
+    desp: "<li>Assisting Musicians to build websites using Javascript languages</li><li>Creating an e-commerce site for a independent fashion brand.</li><li>Personal projects for my own photography and musical collaborations.</li>",
+  },
+  {
+    title: "Musician",
+    cardImage: "assets/images/experience-page/gat3.jpg",
+    place: "Worldwide",
+    time: "(2007 - 2020)",
+    desp:"<li>Worked as a musician on cruise ships, gigs include BB Kings Blues Club, Seabourn, Princess, Crystal, Holland America, Carnival and Royal Caribbean Cruise Lines.</li><li>Worked in Hotels in Dubai and Abu Dhabi.</li><li>Played on tours, bars, restraunts, weddings and festivals in New Zealand, Australia, London, Nashville and Miami.</li>",
+  },
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+      <ul>
+      <li class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </li>
+    </ul>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+// Volunteership Cards
+
+const volunteership = document.querySelector(".volunteership");
+const volunteershipcards = [
+  {
+    title: "GirlScript Summer Of Code 2020",
+    cardImage: "assets/images/experience-page/1.jpg",
+    description:
+      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
+  },
+  {
+    title: "StudentCode-in 2020",
+    cardImage: "assets/images/experience-page/2.jpg",
+    description:
+      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
+  },
+  {
+    title: "PClub Summer Of Code 2020",
+    cardImage: "assets/images/experience-page/3.jpg",
+    description:
+      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
+  },
+  {
+    title: "Hakin-Codes",
+    cardImage: "assets/images/experience-page/4.jpg",
+    description:
+      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
+  },
+  {
+    title: "30 DaysofFlutter",
+    cardImage: "assets/images/experience-page/5.jpg",
+    description:
+      "Helping beginners from their first steps in Flutter to building fully functional cross-platform applications.",
+  },
+  {
+    title: "Garuda Hacks",
+    cardImage: "assets/images/experience-page/6.jpg",
+    description:
+      "MLH sponsored Indonesia's premier Global Virtual Hackathon on 14 - 16th August 2020.",
+  },
+];
+
+const showCards = () => {
+  let output = "";
+  volunteershipcards.forEach(
+    ({ title, cardImage, description }) =>
+      (output += `        
+      <div class="card volunteerCard" style="height: 600px;width:400px">
+      
+      <img src="${cardImage}" height="300" width="65" class="card-img" style="border-radius:10px">
+      <div class="content">
+          <h2 class="volunteerTitle">${title}</h2><br>
+          <p class="copy">${description}</p></div>
+      
+      </div>
+      `)
+  );
+  volunteership.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+// Mentorship Card
+
+
+const mentorshipcards = document.querySelector(".mentorship-cards");
+const mentor = [
+  {
+    title: "HakinCode",
+    image: "assets/images/experience-page/hakin.png",
+    time: "06/2020 - 08/2020",
+    desp: "<li>It is an open source community where students and mentors can apply.</li><hr /><li>Ample amount of technologies and projects are there and we are given opportunity to work on them according to our interest and knowledge.</li>",
+  },
+  {
+    title: "Google Summer of Code",
+    image: "assets/images/experience-page/gsoc.png",
+    time: "03/2020 - 08/2020",
+    desp: "<li>Google Summer of Code is a global program focused on introducing students to open source software development.</li><hr /><li>It is a great platform to explore new areas, maybe discover a new career path!</li>",
+  },
+];
+
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, time, desp}) =>
+      (output += `        
+      <div class="column mentorshipCard"> 
+      <div class="card card2 mentorshipCardCover">
+        <img src="${image}" class="card-img-top" alt="..."  width="64" height="300">
+        <div class="information">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class=""sub-title">${time}</p>
+        </div>
+        <div class="more-information">
+        <ul class="list-group list-group-flush p-0 right-aligned">
+          <div class="list-group-item card2 disclaimer">${desp}</div>
+        </ul>
+        </div>
+        </div>
+      </div>
+      </div>
+      `)
+  );
+  mentorshipcards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
+
+// Programs
+var gage = {
+  init: function(){
+      // restart used for demo purposes - change to $('.gage').each(function(i){
+    $('.chart span').css({"width" : "0"}).parent().each(function(i){
+      // Loop through .gage elements
+      $('p', this).html($(this).attr("data-label"));
+      // Set p html value to the data-label attr set in the element
+      var timeout = parseInt(i) * 60 + 1100;
+      // Set a timeout based on the iteration multiplied by 60 (will affect delay between animations) 
+      $('span', this).delay(timeout).animate({"opacity" : "1"}, 0, function(){
+        //Delay  
+        $(this).css({"width" : $(this).parent().attr("data-level") + "%"});
+      });
+    });
+  }
+}
+
+$(document).ready(function(){
+  // Call gage init function
+  gage.init();
+  // Interval used for demo purposes - remove if using  
+  setInterval(function() {
+      gage.init();
+  }, 5000);
+});
+
